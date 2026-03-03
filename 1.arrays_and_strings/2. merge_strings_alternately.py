705b1b902aee05881e24224753c99d4d4961ec11
Jesus Mendez <mendezpinedaj@gmail.com>
2026-03-03 10:05:54 -0500

    added descrp and sub ulrs

--- 1.arrays_and_strings/2. merge_strings_alternately.py
+++ 1.arrays_and_strings/2. merge_strings_alternately.py
@@ -1,22 +1,59 @@
+"""
+1768. Merge Strings Alternately
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.
+"""
+
+# submittion: https://leetcode.com/problems/merge-strings-alternately/submissions/1936819040/
+
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        res = []
-        n_min = min(len(word1), len(word2))
-        
-        # Merge overlapping parts
-        for i in range(n_min):
-            res.append(word1[i])
-            res.append(word2[i])
-            
-        # Append remaining tails (one will be empty)
-        res.append(word1[n_min:])
-        res.append(word2[n_min:])
-        
-        return "".join(res)
-
-# Test case
-if __name__ == "__main__":
-    solution = Solution()
-    print(solution.mergeAlternately("abc", "pqr"))    # Output: apbqcr
-    print(solution.mergeAlternately("ab", "pqrs"))   # Output: apbqrs
-    print(solution.mergeAlternately("abcd", "pq"))   # Output: apbqcd
+        i = 0
+        word = ""
+        while(i < len(word1) or i < len(word2)):
+            if i < len(word1):
+                word += word1[i]
+            if i < len(word2):
+                word += word2[i]
+            i += 1
+        return word
